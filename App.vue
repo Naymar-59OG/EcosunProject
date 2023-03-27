b2ee44573596a9f60e957486eb4d5892a681e463
Naymar-59OG <71763656+Naymar-59OG@users.noreply.github.com>
2023-03-27 10:29:21 +0200

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,31 @@
+<template>
+  <HeaderButtons />
+  <LoginUser />
+  <FormumairUtilisateur />
+</template>
+
+<script>
+import HeaderButtons from './components/HeaderButtons.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HeaderButtons,
+    
+  },
+};
+</script>
+
+<style>
+body {
+  background-image: url("./assets/logo.PNG");
+  background-size: cover;
+  background-position: center;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 400px;
+}
+</style>
